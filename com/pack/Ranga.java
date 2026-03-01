48748bd02c65fb0edd679195157f5b7cce01d1bc
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2026-03-02 00:53:55 +0530

    add() - main

---
+++ com/pack/Ranga.java
@@ -0,0 +1,15 @@
+package com.pack;
+
+public class Ranga
+{
+    public static void add()
+    {
+        int a=10;
+        int b=20;
+        System.out.println(a+b);
+    }
+
+    public static void main(String[] args) {
+        add();
+    }
+}
